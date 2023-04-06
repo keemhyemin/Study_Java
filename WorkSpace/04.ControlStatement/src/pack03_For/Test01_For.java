ef9e8c17dffcfdd57367ab083f64467e30bf117f
kvmhyvmvn <97383799+kvmhyvmvn@users.noreply.github.com>
2023-04-06 09:49:52 +0900

    구구단 수정

--- WorkSpace/04.ControlStatement/src/pack03_For/Test01_For.java
+++ WorkSpace/04.ControlStatement/src/pack03_For/Test01_For.java
@@ -21,8 +21,11 @@ public class Test01_For {
 		if (num < 2) {
 			System.out.println("잘못된 숫자");
 		} else {
-			for (int i = 1; i <= 9; i++) {
-				System.out.println(num + "x" + i + "=" + num * i);
+			for (int i = 2; i <= num; i++) {
+				System.out.println("**i값: " + i + "**");
+				for (int j = 1; j <= 9; j++) {
+					System.out.println(i + "x" + j + "=" + (i * j));
+				}
 			}
 		}
 		System.out.println("============================================");
