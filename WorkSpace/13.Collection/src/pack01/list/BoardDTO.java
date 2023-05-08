29f812309575853b6f6abc5b34c3eec1f176aded
kvmhyvmvn <97383799+kvmhyvmvn@users.noreply.github.com>
2023-05-08 12:24:23 +0900

    Collection(List, Vector, ArrayList, LinkedList)

---
+++ WorkSpace/13.Collection/src/pack01/list/BoardDTO.java
@@ -0,0 +1,39 @@
+package pack01.list;
+
+public class BoardDTO {
+
+	private int boardNum;
+	private String boardTitle, boardContent;
+
+	public BoardDTO(int boardNum, String boardTitle, String boardContent) {
+		this.boardNum = boardNum;
+		this.boardTitle = boardTitle;
+		this.boardContent = boardContent;
+	}
+
+	public int getBoardNum() {
+		return boardNum;
+	}
+
+	public void setBoardNum(int boardNum) {
+		this.boardNum = boardNum;
+	}
+
+	public String getBoardTitle() {
+		return boardTitle;
+	}
+
+	public void setBoardTitle(String boardTitle) {
+		this.boardTitle = boardTitle;
+	}
+
+	public String getBoardContent() {
+		return boardContent;
+	}
+
+	public void setBoardContent(String boardContent) {
+		this.boardContent = boardContent;
+	}
+
+	
+}
---
+++ WorkSpace/13.Collection/src/pack01/list/Ex03_LinkedList.java
@@ -0,0 +1,35 @@
+package pack01.list;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Ex03_LinkedList {
+	public static void main(String[] args) {
+		// 사용빈도가 낮음. 인덱스끼리 체이닝(연결) 구조를 가지고있어서 장점이 한두가지 있지만
+		// 실제 사용 용도로는 느리고 불편함.(눈으로 확인이 데이터 건수가 낮을 땐 식별불가)
+		
+		// 책 예제: System.nanoTime(); 이라는 메소드를 이용해서 ArrayList vs LinkedList를 비교
+		
+		List<String> listArray = new ArrayList<String>();
+		List<String> listLinked = new LinkedList<String>();
+		
+		long startTime;
+		long endTime;
+		
+		startTime = System.nanoTime();
+		for(int i=0; i<10000; i++) {
+			listArray.add(i+""); // 0번 인덱스에 끼워넣기 처리를 함. Linked가 끼워넣기에는 더 빠르다
+		}
+		endTime = System.nanoTime();
+		System.out.println("ArrayList의 작업시간: " + (endTime - startTime) + "ns");
+		
+		startTime = System.nanoTime();
+		for(int i=0; i<10000; i++) {
+			listLinked.add(i+""); // 0번 인덱스에 끼워넣기 처리를 함. Linked가 끼워넣기에는 더 빠르다
+		}
+		endTime = System.nanoTime();
+		System.out.println("ArrayList의 작업시간: " + (endTime - startTime) + "ns");
+		
+	}
+}
