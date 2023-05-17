e58dcdff1c16550666f891e34c1a39b2cac211d1
kvmhyvmvn <97383799+kvmhyvmvn@users.noreply.github.com>
2023-05-17 12:57:54 +0900

    .

--- WorkSpace/998.Project/src/pack03/Board/BoardDAO.java
+++ WorkSpace/998.Project/src/pack03/Board/BoardDAO.java
@@ -1,12 +1,20 @@
 package pack03.Board;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.jsoup.Jsoup;
 
 public class BoardDAO {
 	Connection conn;
@@ -276,6 +284,72 @@ public class BoardDAO {
 	}
 
 	// ==========================================================================================================================================
+	
+	public void weather() {
+		String[][] tempCategory = { { "T1H", "기온" }, { "RN1", "1시간 내 강수량" }, { "REH", "습도" }, { "PTY", "강수형태" },
+				{ "VEC", "풍향" }, { "WSD", "풍속" } };
+
+		String url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?";
+		String doc = null;
+		String cty; // category
+		// T1H(기온 ℃), RN1(1시간 강수량 mm), REH(습도 %), PTY(강수형태), VEC(풍향 deg), WSD(풍속 m/s)
+		// 강수형태 수치에 대해서 -> 의미없음(0), 비(1), 비/눈(2), 눈(3), 소나기(4), 빗방울(5), 빗방울/눈날림(6),
+		// 눈날림(7)
+
+		Date date = new Date();
+		SimpleDateFormat sdformat = new SimpleDateFormat("yyyyMMdd");
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		String today = sdformat.format(cal.getTime()); // 날짜
+
+		cal.add(Calendar.HOUR, -1); // 현 시간부터 1시간 전
+		SimpleDateFormat sdtime = new SimpleDateFormat("HH" + "00");
+		String hour = sdtime.format(cal.getTime());
+
+		SimpleDateFormat sddate = new SimpleDateFormat("yyyy-MM-dd"); // 날짜 형식 바꾸기
+		cal.setTime(date);
+		String formatdate = sddate.format(cal.getTime());
+
+		cal.add(Calendar.HOUR, -1);
+		SimpleDateFormat sdhour = new SimpleDateFormat("HH시"); // 시간 형식 바꾸기
+		String formathour = sdhour.format(cal.getTime());
+
+		try {
+			doc = Jsoup.connect(url).data("pageNo", "1").data("numOfRows", "10").data("dataType", "Json")
+					.data("base_date", today).data("base_time", hour).data("nx", "58").data("ny", "74")
+					.data("serviceKey",
+							"kmc0ZV1P4EDjawY1Lf6lvCWeZXhgexI8O3A59ZNccISKWGOaO4IIUn+TAO8dYHAhjcPOkkGwsioB7rC1UgPOCQ==")
+					.timeout(1000 * 40).userAgent("chrome").ignoreContentType(true).execute().body();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		JSONParser parser = new JSONParser(); // 문자열 -> JsonObject 형태로 바꿔줌
+		try {
+			JSONObject jsonObj = (JSONObject) parser.parse(doc);
+			jsonObj = (JSONObject) jsonObj.get("response");
+			jsonObj = (JSONObject) jsonObj.get("body");
+			jsonObj = (JSONObject) jsonObj.get("items");
+			JSONArray array = (JSONArray) jsonObj.get("item");
+			System.out.println("날짜:" + formatdate + " | " + "예보시간:" + formathour + " | 위치:광주광역시");
+			System.out.println("========================================");
+			for (int i = 0; i < array.size(); i++) {
+				JSONObject tempObj = (JSONObject) array.get(i);
+				// System.out.println("카테고리 obj"+tempObj.get("category"));
+				for (int j = 0; j < tempCategory.length; j++) {
+					// System.out.println("배열"+tempCategory[j][0]);
+					if (tempObj.get("category").equals(tempCategory[j][0])) {
+						System.out.println(tempCategory[j][1] + ": " + tempObj.get("obsrValue"));
+
+					}
+				}
+
+			}
+		} catch (Exception e) {
+			// String json이 key와 value를 가진 데이터가 아닌 그냥 일반 문자열이라면 오류가 발생.
+			e.printStackTrace();
+		}
+	}
 
 	public void exit(BoardDTO dto) { // 로그아웃
 		System.out.println("프로그램이 종료되었습니다.");
